f8c4f582867fe3887bc8573c4f4c476ec78612d2
ditinex <asifakramsk@gmail.com>
2023-02-28 07:24:05 +0530

    env file updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,7 @@
 const Config = {
-  API_URL: process.env.NEXT_PUBLIC_API_URL //'http://localhost:3001'
-  // STORAGE_URL: process.env.NEXT_PUBLIC_STORAGE_URL,
-  // SQUARE_APPLICATION_ID: process.env.NEXT_PUBLIC_APPLICATION_ID,
-  // SQUARE_LOCATION_ID: process.env.NEXT_PUBLIC_LOCATION_ID,
-  // UNAUTHORIZED_EXCEPTION: false,
-  // VAPID: process.env.NEXT_PUBLIC_VAPID
+  API_URL: process.env.NEXT_PUBLIC_API_URL,
+  STORAGE_URL: process.env.NEXT_PUBLIC_STORAGE_URL,
+  UNAUTHORIZED_EXCEPTION: false
 };
 
 export default Config;
-
-// export const dateOfEstablishment = 2022;
-// export const HoursCoded = 1379;
-// export const HoursBrainstormed = 2200;
-// export const ProjectsDelivered = 39;
-// export const ContactNumber = 9903614705;
-// export const ContactEmail = 'info@ditinex.com';
