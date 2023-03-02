f2c90d40e0b5450cc469027b87f458d408a6adcc
Raees-Mondal9887 <102875335+Raees-Mondal9887@users.noreply.github.com>
2023-03-02 15:57:48 +0530

    done

--- src/layouts/Home/Login.js
+++ src/layouts/Home/Login.js
@@ -6,15 +6,15 @@ import { BsEye, BsEyeSlash } from 'react-icons/bs';
 const Login = ({ _this }) => {
   return (
     <div className="flex-1 flex justify-start">
-      <div className="flex sm:w-[425px] w-full flex-col bg-background justify-evenly items-start p-6">
-        <h1 className=" ml-7 text-4xl text-gray-700 font-medium text-left whitespace-normal break-normal leading-normal">
+      <div className="flex sm:w-[425px] w-full flex-col bg-background justify-start items-start p-8">
+        <h1 className="text-4xl font-medium text-left whitespace-normal break-normal leading-normal text-gray-700">
           Get Started Absolutely Free
         </h1>
-        <h1 className=" ml-7 mt-4 text-xl font-medium text-left text-gray-400">
+        <h1 className="mt-3 text-xl font-medium text-left text-gray-400">
           Sign In To Your Account
         </h1>
 
-        <div className="ml-7">
+        <div className="flex flex-col justify-center items-center my-">
           <div className="flex flex-col justify-center mb-6">
             <label className="text-sm text-gray-500 font-medium mb-2">Business Email Address</label>
             <Input
@@ -55,7 +55,7 @@ const Login = ({ _this }) => {
           >
             Login
           </Button>
-          <p className=" m-2 text-sm font-medium text-center text-gray-400">
+          <p className=" m-2 text-sm font-medium text-center text-gray-500">
             Don't have an account?
             <Link href="/sign-up">
               <span className="cursor-pointer text-primary"> Sign Up</span>
