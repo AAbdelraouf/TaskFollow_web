f6539687b2d82ff9db811c5e37f230c16425c96a
mandar kalangutkar <mkalangutkar13@gmail.com>
2023-03-03 02:17:57 +0530

    settings page

---
+++ src/layouts/Settings/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import Body from './Body';
+
+const Settings = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchedCategories = [
+      'first',
+      'second',
+      'third',
+      'Category 1',
+      'Category 2',
+      'Category 3'
+    ];
+    setCategories(fetchedCategories);
+  }, []);
+
+  const _this = {
+    categories
+  };
+
+  return (
+    <main>
+      <Body _this={_this} />
+    </main>
+  );
+};
+
+export default Settings;
