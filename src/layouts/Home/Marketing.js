74693dd8dce982948be05a3376b74fa3f9f1d672
ditinex <asifakramsk@gmail.com>
2023-03-02 12:33:25 +0530

    alignment fixed

--- src/layouts/Home/Marketing.js
+++ src/layouts/Home/Marketing.js
@@ -3,7 +3,7 @@ import { Button } from 'antd';
 
 const Marketing = () => {
   return (
-    <div className="flex-1 basis-2/4 text-white p-4">
+    <div className="flex-1 text-white p-4">
       <h1>Taskfollow Logo</h1>
       <p>Lorep Lipsum about us</p>
       <Button>Antd</Button>
