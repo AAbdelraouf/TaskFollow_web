3fe092a7c75699921191ac5a5ee06da91f90b98f
ditinex <asifakramsk@gmail.com>
2023-03-02 15:51:31 +0530

    login fixed

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost"],
-  },
+    domains: ['localhost', 'staging-api.taskfollow.net', 'api.taskfollow.net']
+  }
 };
 
 module.exports = nextConfig;
